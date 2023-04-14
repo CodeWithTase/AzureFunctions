8bbb19335a3f50dcf10f7422acb20f712b789463
Tase <erutase.akpobome@wealth-dynamix.com>
2023-04-14 10:04:49 +0100

    added que functionality to add the order to que

--- OnPaymentReceived.cs
+++ OnPaymentReceived.cs
@@ -15,6 +15,7 @@ namespace pluralsightfuncs
         [FunctionName("OnPaymentReceived")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
             ILogger log)
         {
             log.LogInformation("Received a payment.");
@@ -23,6 +24,8 @@ namespace pluralsightfuncs
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var order = JsonConvert.DeserializeObject<Order>(requestBody);
+
+            await orderQueue.AddAsync(order);
 
             log.LogInformation($"Order {order.OrderId} receieved from {order.Email} for product {order.ProductId}");
 
